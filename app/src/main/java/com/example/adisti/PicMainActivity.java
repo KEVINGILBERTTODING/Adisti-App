66afc23c01e6ea6bd1ee37241a6c9557b3908e7a
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-05-10 17:04:59 +0700

    added bottom bar

--- app/src/main/java/com/example/adisti/PicMainActivity.java
+++ app/src/main/java/com/example/adisti/PicMainActivity.java
@@ -1,23 +1,50 @@
 package com.example.adisti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.adisti.PicFragment.PicHomeFragment;
+import com.example.adisti.PicFragment.PicKajianManfaatFragment;
+import com.example.adisti.PicFragment.PicProposalFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class PicMainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_main);
         replace(new PicHomeFragment());
+
+        bottomNavigationView = findViewById(R.id.bottomBar);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menuHome) {
+                    replace(new PicHomeFragment());
+                    return true;
+                }else  if (item.getItemId() == R.id.menuProposal) {
+                    replace(new PicProposalFragment());
+                    return true;
+                }else  if (item.getItemId() == R.id.menuKajianManfaat) {
+                    replace(new PicKajianManfaatFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 
     private void replace(Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(R.id.framePic, fragment)
-                .addToBackStack(null).commit();
+                .commit();
     }
 }
