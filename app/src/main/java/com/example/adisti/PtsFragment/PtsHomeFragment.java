552b00265b485c9c4b49a632334db16229b76ed9
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-05-14 22:18:41 +0700

    added photo profile pts

--- app/src/main/java/com/example/adisti/PtsFragment/PtsHomeFragment.java
+++ app/src/main/java/com/example/adisti/PtsFragment/PtsHomeFragment.java
@@ -1,25 +1,41 @@
 package com.example.adisti.PtsFragment;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.adisti.Model.UserModel;
 import com.example.adisti.PicFragment.PicKajianManfaatFragment;
 import com.example.adisti.PicFragment.PicProposalFragment;
 import com.example.adisti.R;
+import com.example.adisti.Util.DataApi;
+import com.example.adisti.Util.PicInterface;
+
+import es.dmoral.toasty.Toasty;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PtsHomeFragment extends Fragment {
     TextView tvUsername;
     SharedPreferences sharedPreferences;
     CardView cvMenuSurvey, cvMenuHasilSurvey;
+    PicInterface picInterface;
+    String userId;
+    ImageView img_profile;
 
 
 
@@ -60,10 +76,71 @@ public class PtsHomeFragment extends Fragment {
         tvUsername.setText(sharedPreferences.getString("nama", null));
         cvMenuSurvey = view.findViewById(R.id.cvMenuSurvey);
         cvMenuHasilSurvey = view.findViewById(R.id.cvMenuHasilSurvey);
+        img_profile = view.findViewById(R.id.img_profile);
+        picInterface = DataApi.getClient().create(PicInterface.class);
+        userId = sharedPreferences.getString("user_id", null);
+        loadProfile();
+
+
     }
 
     private void replace(Fragment fragment) {
         ((FragmentActivity) getContext()).getSupportFragmentManager().beginTransaction().replace(R.id.framePts, fragment)
                 .addToBackStack(null).commit();
     }
+
+    private void loadProfile(){
+        Dialog dialog = new Dialog(getContext());
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setContentView(R.layout.dialog_progress_bar);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.show();
+
+        picInterface.getUserById(userId).enqueue(new Callback<UserModel>() {
+            @Override
+            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
+                if (response.isSuccessful() && response.body() != null) {
+
+                    Glide.with(getContext()).load(response.body().getPhotoProfile())
+                            .skipMemoryCache(false)
+                            .dontAnimate()
+                            .fitCenter()
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .into(img_profile);
+
+
+
+                    dialog.dismiss();
+
+
+                }else {
+                    Toasty.error(getContext(), "Terjadi kesalahan", Toasty.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserModel> call, Throwable t) {
+                Dialog dialog1 = new Dialog(getContext());
+                dialog1.setCanceledOnTouchOutside(false);
+                dialog1.setContentView(R.layout.dialog_no_connection);
+                dialog1.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                Button btnRefresh;
+                btnRefresh = dialog1.findViewById(R.id.btnRefresh);
+                btnRefresh.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadProfile();
+                        dialog1.dismiss();
+                    }
+                });
+                dialog1.show();
+
+            }
+        });
+
+
+
+    }
+
 }
